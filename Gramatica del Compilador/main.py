86fe5196045b46761df31d0c40e0dabdc5c89462
Jonathan <jjaimeloza5768@gmail.com>
2026-03-02 07:11:50 -0600

    fix: intrucciones en main.py

---
+++ Gramatica del Compilador/main.py
@@ -0,0 +1,5 @@
+"""
+Utilizando tu analizador léxico y tu algoritmo para trabajar con las tablas lr. 
+Carga e implementa la siguiente gramática.
+https://github.com/TraductoresLenguajes2/Traductores/tree/master/Modulo4
+"""
